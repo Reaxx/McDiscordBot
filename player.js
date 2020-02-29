fa5260f51f717125ce841a19871e1707753623e1
Jonny Svensson <swe_jonny@hotmail.com>
2020-02-29 12:29:34 +0100

    Made the notification work with player-object

---
+++ player.js
@@ -0,0 +1,29 @@
+class Player { 
+    constructor(name,FirstLogIn = new Date()) {
+        this.Name = name;
+        this.FirstLogin = FirstLogIn;
+        this.LogIn(FirstLogIn);
+    }
+
+    LogIn(time = new Date()) {
+        this.LastLogin = time;
+        this.IsLoggedIn = true;
+    }
+
+    LogOut(time = new Date()) {
+        this.IsLoggedIn = false;
+        this.LastLogout = time;
+    }
+
+    MinutesLogedIn(time = new Date()) {
+        let diff = (time.getTime() - this.LastLogin.getTime()) / 60000;
+        return Math.round(diff);
+    }
+    
+    MinutesSinceLastLogin(time = new Date()) {
+        let diff = (time.getTime() - this.LastLogout.getTime()) / 60000;
+        return Math.round(diff);
+    }
+}
+
+module.exports = Player
